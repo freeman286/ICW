af931cf3f8da4db9a4d604a8cc6d348a7537e19c
freeman286 <molemayer@gmail.com>
2019-10-28 16:46:49 +0000

    Add Euler integration and fix indexing for timestep

--- numericalint.py
+++ numericalint.py
@@ -3,13 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Mode
-mode = "freq"
+mode = "imp" # imp or freq
+integrator = "euler" # euler or verlet
 
 # Time step
 T = 100
 dt = 0.1
 N = int(T/dt)
-t = np.linspace(0,N*dt,N)
+t = np.linspace(0, N*dt, N)
 
 # Mass matrix
 mf = 1
@@ -33,19 +34,29 @@ Force = 1.0
 x = np.zeros([N,3])
 v = np.zeros([N,3])
 a = np.zeros([N,3])
-x[0],x[1] =[0,0,0],[0,0,0]
-v[0],v[1] =[0,0,0],[0,0,0]
-
+x[0],v[0] =[0, 0, 0], [0, 0, 0]
 
 def run(freq=0):
-    for p in range(2,N):
+    for p in range(1, N):
         if mode == "freq":
-            F = [Force*np.cos(freq*p*dt),0,0]
-        elif mode == "imp" and p == 2:
-            F = [1/dt,0,0]
-        a[p] = np.matmul(np.linalg.inv(M), F - np.matmul(K, x[p-1]) - np.matmul(H, v[p-1])) 
-        x[p] = 2 * x[p-1] - x[p-2] + dt * dt * a[p]
-        v[p] = (x[p] - x[p-1])/dt
+            F = [Force * np.cos(freq * p * dt), 0, 0]
+        elif mode == "imp" and p == 1:
+            F = [1/dt, 0, 0]
+        else:
+            F = [0, 0, 0]
+    
+        if integrator == "verlet":
+            a[p] = np.matmul(np.linalg.inv(M), F - np.matmul(K, x[p-1]) - np.matmul(H, v[p-1])) 
+            if p != 1:
+                x[p] = 2 * x[p-1] - x[p-2] + dt * dt * a[p]
+            else:
+                x[p] = 2 * x[p-1] + dt * dt * a[p]
+            v[p] = (x[p] - x[p-1])/dt
+
+        elif integrator == "euler":
+            a[p] = np.matmul(np.linalg.inv(M), F - np.matmul(K, x[p-1]) - np.matmul(H, v[p-1])) 
+            v[p] = v[p-1] + a[p]*dt
+            x[p] = x[p-1] + v[p]*dt
 
 
 def max_amplitude(freq):
@@ -54,20 +65,24 @@ def max_amplitude(freq):
     return max_amp
 
 if mode == "freq":
-    freq_sweep = np.linspace(0.1,3,30)
-    response = np.array([ max_amplitude(freq) for freq in freq_sweep])
-
+    freq_sweep = np.linspace(0.1, 3, 30)
+    response = np.array([max_amplitude(freq) for freq in freq_sweep])
     lines = plt.plot(freq_sweep,response)
+
     plt.legend(lines, ('floor1', 'floor2', 'damper'))
     plt.xlabel('Frequency (Hz)', fontsize=10)
     plt.ylabel('Amplitude (m)', fontsize=10)
+
     plt.show()
+    
 elif mode == "imp":
     run()
     lines = plt.plot(t, x)
+
     plt.legend(lines, ('floor1', 'floor2', 'damper'))
     plt.xlabel('Time (s)', fontsize=10)
     plt.ylabel('Amplitude (m)', fontsize=10)
+
     plt.show()
 
 
